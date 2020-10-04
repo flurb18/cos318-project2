35b21957444c23fd26a77fc64a6b64f31bf554e2
Aidan Donahue <aidand@princeton.edu>
2020-10-04 19:27:17 -0400

    implemented timing of do_yield and yield()

--- process3.c
+++ process3.c
@@ -7,6 +7,19 @@
 #include "syslib.h"
 #include "util.h"
 
+static bool_t FIRST_RUN = TRUE;
+static uint64_t start = 0;
+
 void _start(void) {
+  uint64_t finish = get_timer();
+  if (FIRST_RUN) {
+    start = get_timer();
+    FIRST_RUN = FALSE;
+    yield();
+  } else {
+    print_str(0,20,"yield(): ");
+    print_int(0,29,(finish - start));
     exit();
+  }
+  exit();
 }
--- th3.c
+++ th3.c
@@ -7,10 +7,20 @@
 #include "scheduler.h"
 #include "util.h"
 
+uint64_t start;
+uint64_t finish;
+
+// IMPORTANT: thread4 must immediately precede thread5
+
 void thread4(void) {
+  start = get_timer();
+  do_yield();
   do_exit();
 }
 
 void thread5(void) {
+  finish = get_timer();
+  print_str(0,0,"do_yield: ");
+  print_int(0,10,(finish - start));
   do_exit();
 }
